65cd707b2123616ed221daa7ce56b6152f4cf6fb
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2023-10-29 19:55:51 -0300

    Update busca_binaria.dart

--- busca_binaria/busca_binaria.dart
+++ busca_binaria/busca_binaria.dart
@@ -1,1 +1,24 @@
+void main() {
+  var lista = new List<int>.generate(1000000, (i) => i + 1);
+  var r = buscaBinaria(lista, 1);
+  print(lista[r]);
+}
 
+buscaBinaria(List<int> list, int item) {
+  int baixo = 0;
+  int alto = list.length - 1;
+
+  while (baixo <= alto) {
+    int meio = (baixo + alto) ~/ 2;
+    int chute = list[meio.toInt()];
+
+    if (chute == item) return meio;
+    if (chute > item) {
+      alto = meio - 1;
+    } else {
+      baixo = meio + 1;
+    }
+  }
+
+  return null;
+}
