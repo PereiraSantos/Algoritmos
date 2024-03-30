2968afad046f2bfe62771b210153a1d702a11d07
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2024-03-30 17:36:25 -0300

    Update quick_sort.dart

--- quick_sort/quick_sort.dart
+++ quick_sort/quick_sort.dart
@@ -1,1 +1,28 @@
+import 'dart:math';
+
+void main() {
+  var rng = new Random();
+  var lista = new List<int>.generate(100, (_) => rng.nextInt(1000));
+  print(quickSort(lista));
+}
+
+quickSort(List<int> list) {
+  if (list.length < 2) {
+    return list;
+  } else {
+    int pivo = list[0];
+    List<int> menores = [];
+    List<int> maiores = [];
+
+    for (var i = 1; i < list.length; i++) {
+      if (list[i] <= pivo) menores.add(list[i]);
+    }
+
+    for (var i = 1; i < list.length; i++) {
+      if (list[i] > pivo) maiores.add(list[i]);
+    }
+
+    return [...quickSort(menores), pivo, ...quickSort(maiores)];
+  }
+}
 
