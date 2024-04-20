5fef502bc847150fcc1ac16f56f935e6890897e4
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2024-04-20 19:59:37 -0300

    Create pesquisa_binaria.ts

---
+++ pesquisa_binaria/pesquisa_binaria.ts
@@ -0,0 +1,31 @@
+function pesquisaBinaria(array:number [], item: number): number | undefined {
+    let menor = 0
+    let maior = array.length - 1
+
+    while(menor <= maior){
+        let meioIndex = Math.floor((menor + maior) / 2)
+        let meio = array[meioIndex]
+
+        if (meio == item ) return meioIndex
+
+        if (meioIndex > item){
+            maior = meioIndex - 1
+        } else {
+            menor = meioIndex + 1
+        }
+    }
+
+    return undefined
+}
+
+const list:number[] = []
+
+for (let i = 0; i < 10000; i++) {
+    list.push(i)
+}
+
+var result = pesquisaBinaria(list, 900)
+
+result === undefined 
+    ? console.log("Número não encontrado") 
+    : console.log(list[result])
