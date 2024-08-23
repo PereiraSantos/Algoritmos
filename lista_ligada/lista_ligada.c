7b25f2745772b07d245ada57711502ec1269f209
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2024-08-22 21:31:32 -0300

    Create lista_ligada.c

---
+++ lista_ligada/lista_ligada.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef int bool;
+typedef int TIPOCHAVE;
+
+typedef struct
+{
+    TIPOCHAVE chave;
+
+} REGISTRO;
+
+typedef struct aux
+{
+    REGISTRO reg;
+    struct aux *prox;
+} ELEMENTO;
+
+typedef ELEMENTO *PONT;
+
+typedef struct
+{
+    PONT inicio;
+} LISTA;
+
+void iinicializarLista(LISTA *l)
+{
+    l->inicio = NULL;
+}
+
+int tamanho(LISTA *l)
+{
+    PONT end = l->inicio;
+    int tam = 0;
+
+    while (end != NULL)
+    {
+        tam++;
+        end = end->prox;
+    }
+
+    return tam;
+}
+
+void exibirLista(LISTA *l)
+{
+    PONT end = l->inicio;
+    printf("Lista: \"");
+    while (end != NULL)
+    {
+        printf("%i ", end->reg.chave);
+        end = end->prox;
+    }
+
+    printf("\"\n");
+}
+
+PONT buscaSequencial(LISTA *l, TIPOCHAVE ch)
+{
+    PONT pos = l->inicio;
+    while (pos != NULL)
+    {
+        if (pos->reg.chave == ch)
+            return pos;
+        pos = pos->prox;
+    }
+
+    return NULL;
+}
+
+PONT buscaSequencialOrd(LISTA *l, TIPOCHAVE ch)
+{
+    PONT pos = l->inicio;
+    while (pos != NULL && pos->reg.chave < ch)
+    {
+        pos = pos->prox;
+    }
+
+    if (pos != NULL && pos->reg.chave == ch)
+        return pos;
+
+    return NULL;
+}
+
+PONT buscaSequencialExc(LISTA *l, TIPOCHAVE ch, PONT *ant)
+{
+    *ant = NULL;
+    PONT atual = l->inicio;
+    while ((atual != NULL) && (atual->reg.chave < ch))
+    {
+        *ant = atual;
+        atual = atual->prox;
+    }
+    if ((atual != NULL) && (atual->reg.chave == ch))
+        return atual;
+    return NULL;
+}
+
+bool inserirElemento(LISTA *l, REGISTRO reg)
+{
+    TIPOCHAVE ch = reg.chave;
+    PONT ant, i;
+    i = buscaSequencialExc(l, ch, &ant);
+    if (i != NULL)
+        return 0;
+    i = (PONT)malloc(sizeof(ELEMENTO));
+    i->reg = reg;
+    if (ant == NULL)
+    {
+        i->prox = l->inicio;
+        l->inicio = i;
+    }
+    else
+    {
+        i->prox = ant->prox;
+        ant->prox = i;
+    }
+
+    return 1;
+}
+
+bool excluirElemLista(LISTA *l, TIPOCHAVE ch)
+{
+    PONT ant, i;
+    i = buscaSequencialExc(l, ch, &ant);
+    if (i == NULL)
+        return 0;
+    if (ant == NULL)
+        l->inicio = i->prox;
+    else
+        ant->prox = i->prox;
+
+    free(i);
+    return 1;
+}
+
+void reinicializarLista(LISTA *l)
+{
+    PONT end = l->inicio;
+    while (end != NULL)
+    {
+        PONT apagar = end;
+        end = end->prox;
+        free(apagar);
+    }
+    l->inicio = NULL;
+}
