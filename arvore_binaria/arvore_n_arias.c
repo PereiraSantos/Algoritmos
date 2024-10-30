b3ae21206997042b9baec581b891edf8213c3c3c
Marciano Pereira <43121709+PereiraSantos@users.noreply.github.com>
2024-10-29 21:07:48 -0300

    Create arvore_n_arias.c

---
+++ arvore_binaria/arvore_n_arias.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+typedef int bool;
+typedef int TIPOCHAVE;
+
+typedef struct no
+{
+    TIPOCHAVE chave;
+    struct no *primFilho;
+    struct no *proxIrmao;
+} NO;
+
+typedef NO *PONT;
+
+PONT criaNovoNo(TIPOCHAVE ch)
+{
+    PONT novo = (PONT)malloc(sizeof(NO));
+    novo->primFilho = NULL;
+    novo->primFilho = NULL;
+    novo->chave = ch;
+    return (novo);
+}
+
+PONT inicializa(TIPOCHAVE ch)
+{
+    return (criaNovoNo(ch));
+}
+
+bool insere(PONT raiz, TIPOCHAVE novaChave, TIPOCHAVE chavePai)
+{
+    PONT pai = buscaChave(chavePai, raiz);
+    if (!pai)
+        return (false);
+    PONT filho = criaNovoNo(novaChave);
+    PONT p = pai->primFilho;
+    if (!p)
+        pai->primFilho = filho;
+    else
+    {
+        while (p->proxIrmao)
+        {
+            p = p->proxIrmao;
+        }
+        p->proxIrmao = filho;
+    }
+    return (true);
+}
+
+bool buscaChave(TIPOCHAVE ch, PONT raiz)
+{
+    if (raiz == NULL)
+        return NULL;
+    if (raiz->chave == ch)
+        return NULL;
+    PONT p = raiz->primFilho;
+    while (p)
+    {
+        PONT resp = buscaChave(ch, p);
+        if (resp)
+            return (resp);
+        p = p->proxIrmao;
+    }
+    return NULL;
+}
+
+void exibirArvore(PONT raiz)
+{
+    if (raiz == NULL)
+        return;
+    printf("%d(", raiz->chave);
+    PONT p = raiz->primFilho;
+    while (p)
+    {
+        exibirArvore(p);
+        p = p->proxIrmao;
+    }
+
+    printf(")");
+}
